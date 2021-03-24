2bab414f339436a14edaf8a147610336a4bd25e2
luis <luis.louis.castro@gmail.com>
2021-03-23 22:00:23 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use NodeJS base image
+FROM node:14.16.0-alpine3.10
+
+WORKDIR usr/src/app
+
+ADD package.json /usr/src/app
+
+RUN npm install --production -no-package-lock
+
+RUN npm install -g typescript
+
+COPY . .
+
+RUN tsc
+
+EXPOSE 8080
+
+CMD ["npm", "run", "prod"]
---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+POSTGRES_USERNAME=postgres
+POSTGRES_PASSWORD=Castillo0310
+POSTGRES_HOST=database-1.cofim9mrqzvz.us-east-1.rds.amazonaws.com
+POSTGRES_DB=postgres
+AWS_BUCKET=test-013837515666-dev
+AWS_REGION=us-east-1
+AWS_PROFILE=default
+JWT_SECRET=biche
+URL=http://localhost:8080
+PORT=8080
